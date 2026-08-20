96df9ac201cc3a32f71fbcb69b46ca3fbe09a076
Takumu Soma <128652629+Healthy0128@users.noreply.github.com>
2026-08-20 18:23:16 +0900

    Add PWA service worker

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'skybound-v2';
+const LOCAL = ['./','./index.html','./css/style.css','./js/controls.js','./js/game.js','./manifest.webmanifest','./icon.svg'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(LOCAL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(hit => hit || fetch(event.request).then(response => {
+      if (new URL(event.request.url).origin === location.origin) {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+      }
+      return response;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
