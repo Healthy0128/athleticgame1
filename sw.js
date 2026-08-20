9f2d2b6e405d23afeebdd003ce35297f9e53cf81
Takumu Soma <128652629+Healthy0128@users.noreply.github.com>
2026-08-20 21:51:01 +0900

    Refresh PWA cache for full-power expansion

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='skybound-v7';
-const LOCAL=['./','./index.html','./css/style.css','./js/game-data.js','./js/game.js','./js/game-render.js','./js/game-input.js','./js/game-phase2.js','./js/game-fixes.js','./js/boot.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='skybound-v8';
+const LOCAL=['./','./index.html','./css/style.css','./js/game-data.js','./js/game.js','./js/game-render.js','./js/game-input.js','./js/game-phase2.js','./js/game-fixes.js','./js/game-fullpower.js','./js/boot.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(LOCAL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{if(new URL(e.request.url).origin===location.origin){const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));}return r;}).catch(()=>caches.match(e.request).then(hit=>hit||caches.match('./index.html'))));});
